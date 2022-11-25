b9e75de16348a6022104cd44983092b81aa86fbe
sunnatbek07 <qadamovsunnatbek07@gmail.com>
2022-11-25 19:25:39 +0500

    add

--- main.c
+++ main.c
@@ -1,10 +1,35 @@
 #include <stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+void fill_random_nums(int nums[], int n){
+    for(int i = 0; i < n; i++){
+        nums[i] = 8 + rand() % 21;
+    }
+}
+
+void output(int nums[], int n){
+    for(int i = 0; i < n; i++){
+        printf("%2d ", nums[i]);
+    }
+    puts("");
+}
+
+
 
 int main(){
+    srand(time(0));
 
-    printf("Hello");
-    printf("Hello");
-    printf("World");
+    int n;
+
+    printf("n = "); scanf("%d", &n);
+
+    int nums[n];
+
+    fill_random_nums(nums, n);
+    output(nums, n);
+    select_sort(nums, n);
+    output(nums, n);
 
     return 0;
 }
